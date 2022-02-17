2a8d37652753ea804777c9bdc5b5bd9ca715cdad
Shakiba Alam <shakibaalam092@gmail.com>
2022-02-17 13:10:26 +0600

    fourth commit

--- code.js
+++ code.js
@@ -1,9 +1,18 @@
-
+// input id and for error handling
 function getInput(id) {
     const inputField = document.getElementById(id);
     const inputValue = parseFloat(inputField.value);
+    if (inputValue < 0) {
+        alert('please put positive number');
+        document.getElementById('expense').style.display = 'none';
+        document.getElementById('balance').style.display = 'none';
+        document.getElementById('save-amount').style.display = 'none';
+        document.getElementById('remaining-balance').style.display = 'none';
+    }
     return inputValue;
 }
+
+//calculate part
 
 document.getElementById('calculate').addEventListener('click', function () {
 
@@ -18,28 +27,18 @@ document.getElementById('calculate').addEventListener('click', function () {
     const balanceAmount = getInput('income') - totalExpense;
     balance.innerText = balanceAmount;
 
-
 })
 
-// function savings(balance) {
-//     //saving amount
-//     const savingAmount = document.getElementById('save-amount');
-//     const percentage = getInput('income') * (20 / 100);
-//     savingAmount.innerText = percentage;
-//     //remaining amount
-//     const remaining = document.getElementById('remaining-balance');
-//     const remainingAmount = balance - savingAmount;
-//     remaining.innerText = remainingAmount;
-// }
+//savings part
 
-document.getElementById('save').addEventListener('click', function (event) {
+document.getElementById('save').addEventListener('click', function () {
     //saving amount
     const savingAmount = document.getElementById('save-amount');
     const percentage = getInput('income') * (20 / 100);
     savingAmount.innerText = percentage;
     //remaining amount
     const remaining = document.getElementById('remaining-balance');
-    const remainingAmount = event.target.balance - savingAmount;
+    const remainingAmount = document.getElementById('balance').innerText - percentage;
     remaining.innerText = remainingAmount;
 
 })
